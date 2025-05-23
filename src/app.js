b9253023b0e40c6266793a26b3cb44dce36e1b35
mallikarjunPC <mallikarjunahi@gmail.com>
2025-05-23 20:19:12 +0530

    Exploring the routes with postman

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const express = require("express");
 
 const server = express();
+const PORT = 3000;
 
-server.listen(3000,"0.0.0.0",()=>{
+server.listen(PORT,"0.0.0.0",()=>{
     console.log("I am listening now");
+})
+
+server.get("/user", (req,res)=>{
+    res.send({"userName":"Sona"})
+})
+
+server.post("/user", (req,res)=>{
+    // res.send({"userName":"Sona"})
+    console.log("user data saved successfully")
+})
+
+server.delete("/user", (req,res)=>{
+    res.send({"userName":"Sona"})
+    console.log("user deleted")
 })
