92de25718aa12ef7397ce3bc0c5599fdec2dd23d
mallikarjunPC <mallikarjunahi@gmail.com>
2025-05-24 16:01:28 +0530

    tried multiple route handlers

--- src/app.js
+++ src/app.js
@@ -7,17 +7,21 @@ server.listen(PORT,"0.0.0.0",()=>{
     console.log("I am listening now");
 })
 
-// using the regx here
-// server.get(/^\/ab?c$/,(req,res)=>{
-//     res.send("yes here is your data")
-// })
+function handler1 (rq,rs,next){
+    console.log("somebody is requesting")
+    next()
+}
 
-server.get("/user",(req,res)=>{
-    console.log(req.query)
-    res.send("yes here is your data")
-})
+function handler2 (req,res,next){
+    res.send("serving via 2nd handler")
+    console.log("Now control is with second handler")
+    next()
+}
 
-server.get("/user/:userid",(req,res)=>{
-    console.log(req.params)
-    res.send("yes here is your data")
-})
+function handler3 (req,res,next){
+    // res.send("serving via 2nd handler")
+    console.log("Now control is with 3rd handler")
+    next()   
+}
+
+server.get("/user",handler1,handler3,handler2)
