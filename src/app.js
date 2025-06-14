8aba45d4ed7304d580cf2423e1873f30d12897ff
mallikarjunPC <mallikarjunahi@gmail.com>
2025-06-14 21:54:45 +0530

    request handlers for users GET POST PATCH DELETE

--- src/app.js
+++ src/app.js
@@ -33,3 +33,61 @@ server.post("/signup",async (req,res)=>{
         console.log("This is the error message ",err)
     }
 })
+
+server.get("/feed", async(req,res)=>{
+    try{
+        res.send(await User.find({}));
+    } catch(err){
+        res.status(400).send("Error getting the feed data")
+        console.log("This is the error message ",err)
+    }
+})
+
+server.get("/user", async(req,res)=>{
+    const userMail = req.body.email;
+    try{
+        const user = await User.findOne({ email: userMail});
+        if(user===null){
+            res.status(400).send("No user found")
+        } else{
+            res.send(user);
+        }
+    } catch(err){
+        res.status(400).send("something went wrong")
+        console.log("This is the error message ",err)
+    }
+})
+
+server.delete("/user",async(req,res)=>{
+    const userMail = req.body.email;
+    try{
+        const user = await User.findOne({ email: userMail});
+        if(user===null){
+            res.status(400).send("No user found")
+        } else{
+            // await User.deleteMany({ email: userMail})
+            // await User.deleteMany({ email: userMail})
+            await User.findByIdAndDelete(user._id.toString())
+            res.send(`User has been deleted: ${userMail}`);
+        }
+    } catch(err){
+        res.status(400).send("something went wrong while deleting user")
+        console.log("This is the error message ",err)
+    }
+})
+
+server.patch("/user",async(req,res)=>{
+    const userMail = req.body.email;
+    try{
+        const user = await User.findOne({ email: userMail});
+        if(user===null){
+            res.status(400).send("No user found")
+        } else{
+            await User.findByIdAndUpdate(user._id.toString(),req.body,{returnDocument:'after'})
+            res.send(`User has been updated: ${userMail}`);
+        }
+    } catch(err){
+        res.status(400).send("something went wrong while updating user")
+        console.log("This is the error message ",err)
+    }
+})
